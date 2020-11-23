4d5af10c01343fd61732e348bd66bf64f2d958b4
Angelika Kuleta <kuleta.an@gmail.com>
2020-11-23 09:20:13 +0100

    Initial commit

---
+++ Hangman/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartGame();
+        }
+
+        static void StartGame()
+        {
+            Console.WriteLine("Satrting game...");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# dotnet-console-hangman
+
+Console based Hangman game with a list of countries-capitals words to play with.
+
+To run in Visual Studio Code, type
+
+```
+dotnet run
+````
